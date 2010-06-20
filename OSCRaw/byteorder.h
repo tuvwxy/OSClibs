42b345582cc7a64a5b8a7930f7e124f05723f6ea
Toshiro Yamada <tnyamada@gmail.com>
2010-06-20 13:41:32 +0300

    updated byteorder.h to include keyword inline

--- OSCRaw/byteorder.h
+++ OSCRaw/byteorder.h
@@ -1,6 +1,5 @@
 /*
  *  byteorder.h
- *  CoviseAudio
  *
  *  Created by Toshiro Yamada on 04/02/10.
  *  Copyright 2010 Calit2, UCSD. All rights reserved.
@@ -21,18 +20,18 @@
 #define ntohll(x) (((int64_t)(ntohl((int32_t)((x << 32) >> 32))) << 32) | \
 						(uint32_t)ntohl(((int32_t)(x >> 32)))) //By Runner
 //#define ntohf(x) *(float*)&(ntohl(x))
-double ntohf(int32_t x) { x = ntohl(x); return *(float*)&x; }
+inline double ntohf(int32_t x) { x = ntohl(x); return *(float*)&x; }
 //#define ntohd(x) (double)ntohll(x)
-double ntohd(int64_t x) { return (double)ntohll(x); }
+inline double ntohd(int64_t x) { return (double)ntohll(x); }
 
 
 //	Host to Network
 
 #define htonll(x) ntohll(x)
 //#define htonf(x) (int32_t)htonl(*(int32_t*)&x)
-int32_t htonf(float x) { return (int32_t)htonl(*(int32_t*)&x); }
+inline int32_t htonf(float x) { return (int32_t)htonl(*(int32_t*)&x); }
 //#define htond(x) (int64_t)htonll(*(int64_t*)&x)
-int64_t htond(double x) { return (int64_t)htonll(*(int64_t*)&x); }
+inline int64_t htond(double x) { return (int64_t)htonll(*(int64_t*)&x); }
 
 
 #endif // __BYTEORDER__
--- oscpack/byteorder.h
+++ oscpack/byteorder.h
@@ -1,6 +1,5 @@
 /*
  *  byteorder.h
- *  CoviseAudio
  *
  *  Created by Toshiro Yamada on 04/02/10.
  *  Copyright 2010 Calit2, UCSD. All rights reserved.
@@ -14,13 +13,17 @@
 #include <arpa/inet.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 //	Network to Host 
 
 // Following ntohll() and htonll() code snipits were taken from
 // http://www.codeproject.com/KB/cpp/endianness.aspx?msg=1661457
 #define ntohll(x) (((int64_t)(ntohl((int32_t)((x << 32) >> 32))) << 32) | \
-(uint32_t)ntohl(((int32_t)(x >> 32)))) //By Runner
-#define ntohf(x) (float)ntohl(x)
+						(uint32_t)ntohl(((int32_t)(x >> 32)))) //By Runner
+//#define ntohf(x) (float)ntohl(x)
+inline double ntohf(int32_t x) { x = ntohl(x); return *(float*)&x; }
 //#define ntohd(x) (double)ntohll(x)
 inline double ntohd(int64_t x) { return (double)ntohll(x); }
 
@@ -33,5 +36,8 @@ inline int32_t htonf(float x) { return (int32_t)htonl(*(int32_t*)&x); }
 //#define htond(x) (int64_t)htonll(*(int64_t*)&x)
 inline int64_t htond(double x) { return (int64_t)htonll(*(int64_t*)&x); }
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif // __BYTEORDER__
